2ed33b387ea707ae657a0b377ad33fde44b082e9
Sambhav196 <sambhav.siwakoti196@gmail.com>
2025-12-29 12:18:46 -0600

    First commit is here

---
+++ src/main/java/org/example/spring/springpractice/SpringPracticeApplication.java
@@ -0,0 +1,14 @@
+package org.example.spring.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeApplication.class, args);
+    }
+    
+
+}
